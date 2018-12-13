2a1ed4be0118472b841fc747887eea9e1e5f9bcb
Jacob Perron <jacob@openrobotics.org>
2018-12-12 21:29:25 -0800

    Account for potential target namespace when finding cmake module... (#2)
    * Account for potential target namespace when finding cmake module tinyxml2
    
    * Set TinyXML2_FOUND

--- cmake/Modules/FindTinyXML2.cmake
+++ cmake/Modules/FindTinyXML2.cmake
@@ -6,10 +6,14 @@
 find_package(TinyXML2 CONFIG QUIET)
 if(TinyXML2_FOUND)
   message(STATUS "Found TinyXML2 via Config file: ${TinyXML2_DIR}")
-  if(NOT TINYXML2_LIBRARY AND TARGET tinyxml2)
+  if(NOT TINYXML2_LIBRARY)
     # in this case, we're probably using TinyXML2 version 5.0.0 or greater
     # in which case tinyxml2 is an exported target and we should use that
-    set(TINYXML2_LIBRARY tinyxml2)
+    if(TARGET tinyxml2)
+      set(TINYXML2_LIBRARY tinyxml2)
+    elseif(TARGET tinyxml2::tinyxml2)
+      set(TINYXML2_LIBRARY tinyxml2::tinyxml2)
+    endif()
   endif()
 else()
   find_path(TINYXML2_INCLUDE_DIR NAMES tinyxml2.h)
@@ -20,6 +24,7 @@ else()
   find_package_handle_standard_args(TinyXML2 DEFAULT_MSG TINYXML2_LIBRARY TINYXML2_INCLUDE_DIR)
 
   mark_as_advanced(TINYXML2_INCLUDE_DIR TINYXML2_LIBRARY)
+  set(TinyXML2_FOUND ${tinyxml2_FOUND})
 endif()
 
 # TODO: Determine if this is necessary with mixed-case find_package_handle_standard_args.
